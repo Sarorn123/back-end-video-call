6ca8d849ffdd5b336f8eb5b6afc89f3e556da201
rorn <rysarorn8@gmail.com>
2022-09-14 17:34:42 +0700

    fie

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
 	cors: {
-		origin: "http://localhost:3000"
+		origin: "https://sparkling-kashata-d148ad.netlify.app"
 	}
 });
 
