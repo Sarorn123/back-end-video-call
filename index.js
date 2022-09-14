65f052aa605d4e99712506c80b07ca17ec5b324a
rorn <rysarorn8@gmail.com>
2022-09-14 17:20:37 +0700

    d

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+	cors: {
+		origin: "http://localhost:3000"
+	}
+});
+
+io.on("connection", (socket) => {
+	socket.on("join-room", (room, user_id) => {
+		socket.join(room);
+
+		console.log("room => ", room);
+		console.log("user_id => ", user_id);
+
+		socket.broadcast.to(room).emit('user-join', user_id);
+
+		socket.on("disconnect", () => {
+			socket.broadcast.to(room).emit('user-gone', user_id);
+		})
+	});
+});
+
+httpServer.listen(8000, () => {
+	console.log("server running on port " + 8000);
+});
